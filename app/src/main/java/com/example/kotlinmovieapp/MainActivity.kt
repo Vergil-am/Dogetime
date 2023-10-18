9a244233fb603b2896e7d7f974055cbc0bbc58ef
amine <madmanamine2@gmail.com>
2023-10-18 21:02:01 +0100

    Created the Nav bar

--- app/src/main/java/com/example/kotlinmovieapp/MainActivity.kt
+++ app/src/main/java/com/example/kotlinmovieapp/MainActivity.kt
@@ -1,29 +1,28 @@
 package com.example.kotlinmovieapp
 
 import android.os.Bundle
-import android.text.Layout
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.material.icons.outlined.Favorite
+import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.Person
-import androidx.compose.material3.BottomAppBar
+import androidx.compose.material.icons.outlined.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.kotlinmovieapp.ui.theme.KotlinMovieAppTheme
 
@@ -32,7 +31,6 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             KotlinMovieAppTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
@@ -44,43 +42,36 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+class Item(val icon: ImageVector, val title: String)
+val Items = listOf(
+    Item(icon = Icons.Outlined.Home, "Home"),
+    Item(icon = Icons.Outlined.Search, "Search"),
+    Item(icon = Icons.Outlined.FavoriteBorder, "Favorites"),
+    Item(icon = Icons.Outlined.Person, "Account"),
+
+)
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Main() {
-    Scaffold (
-        bottomBar = {
-            BottomAppBar (
-                containerColor = MaterialTheme.colorScheme.primaryContainer,
-                contentColor = MaterialTheme.colorScheme.primary,
-                actions = {
-                        IconButton(onClick = { /*TODO*/ }) {
-                            Icon(
-                                Icons.Outlined.Home,
-                                contentDescription = "Home"
-                            )
-                        }
-                        IconButton(onClick = { /*TODO*/ }) {
-                            Icon(
-                                Icons.Filled.Search,
-                                contentDescription = "Search"
-                            )
-                        }
-                        IconButton(onClick = { /*TODO*/ }) {
-                            Icon(
-                                Icons.Outlined.Favorite,
-                                contentDescription = "Favorites"
-                            )
-                        }
-                        IconButton(onClick = { /*TODO*/ }) {
-                            Icon(
-                                Icons.Outlined.Person,
-                                contentDescription = "Account"
-                            )
-                        }
+    val selectedItem = remember { mutableStateOf(0) }
 
+    Scaffold (
+
+        bottomBar = {
+            NavigationBar (
+            ) {
+                Items.forEachIndexed { index, item: Item ->
+                    NavigationBarItem(
+                        selected = selectedItem.value == index,
+                        onClick = {
+                            selectedItem.value = index
+                        },
+                        label = {item.title},
+                        icon = { Icon(item.icon, item.title)})
 
                 }
-            )
+            }
         }
     )
     {innerPadding ->
