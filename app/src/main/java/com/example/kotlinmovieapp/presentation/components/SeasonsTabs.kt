2a692affcd99da59f8802e9a22bb0521c3330f77
amine <madmanamine2@gmail.com>
2023-11-05 16:57:29 +0100

    season tabs

--- app/src/main/java/com/example/kotlinmovieapp/presentation/components/SeasonsTabs.kt
+++ app/src/main/java/com/example/kotlinmovieapp/presentation/components/SeasonsTabs.kt
@@ -1,17 +1,26 @@
 package com.example.kotlinmovieapp.presentation.components
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.ScrollableTabRow
 import androidx.compose.material3.Tab
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import com.example.kotlinmovieapp.domain.model.Season
 import com.example.kotlinmovieapp.presentation.details.DetailsViewModel
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun SeasonsTabs(
     seasons: List<Season>,
@@ -19,9 +28,19 @@ fun SeasonsTabs(
     viewModel: DetailsViewModel
 ) {
     var selectedTab by remember { mutableIntStateOf(0) }
+    val pagerState = rememberPagerState (
+        pageCount = { seasons.count() }
+    )
+    LaunchedEffect(selectedTab) {
+        pagerState.animateScrollToPage(selectedTab)
+    }
+    LaunchedEffect(pagerState.currentPage) {
+        selectedTab = pagerState.currentPage
+    }
     val state = viewModel.state.collectAsState()
     ScrollableTabRow(
         selectedTabIndex = selectedTab,
+        modifier = Modifier.fillMaxWidth(),
 
         ) {
         seasons.forEachIndexed { index, season ->
@@ -30,12 +49,27 @@ fun SeasonsTabs(
                 onClick = {
                     selectedTab = index
                     viewModel.getSeason(id, season.season_number)
-                }) {
+                }
+            ) {
                 Text(text = season.season_number.toString())
-
             }
         }
 
     }
+        HorizontalPager(state = pagerState,
+            modifier = Modifier.fillMaxSize()
+        ) {
+                page ->
+            Box(modifier = Modifier
+            ) {
+                val season = seasons[page]
+                viewModel.getSeason(id, season.season_number)
+
+
+
+            }
+        }
+
+
 
 }
