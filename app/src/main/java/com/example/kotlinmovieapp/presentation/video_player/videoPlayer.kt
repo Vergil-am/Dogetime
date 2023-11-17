0bf0a0041bb982b3f3d0090759533065ff08184c
amine <madmanamine2@gmail.com>
2023-11-17 17:08:35 +0100

    set webview to display in landscape and it can go fullscreen now

--- app/src/main/java/com/example/kotlinmovieapp/presentation/video_player/videoPlayer.kt
+++ app/src/main/java/com/example/kotlinmovieapp/presentation/video_player/videoPlayer.kt
@@ -1,14 +1,22 @@
 package com.example.kotlinmovieapp.presentation.video_player
 import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.pm.ActivityInfo
+import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebChromeClient
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.FrameLayout
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
@@ -24,25 +32,34 @@ fun VideoPlayer(
 ) {
    windowCompat.hide(WindowInsetsCompat.Type.systemBars())
    windowCompat.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-//   val context = LocalContext.current
-//   var orientation = LocalConfiguration.current.orientation
+   val activity = LocalView.current.context as Activity
+   val isFullscreen = remember {
+      mutableStateOf(false)
+   }
    val url: String = if (season != 0 && episode != 0) {
       "https://vidsrc.to/embed/tv/$id/$season/$episode"
    } else {
       "https://vidsrc.to/embed/movie/$id"
    }
-   Box (
+   DisposableEffect(key1 = activity ) {
+      activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+      onDispose {
+         activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED
+      }
+   }
+
+      AndroidView(
          modifier = Modifier
-            .fillMaxSize()
-         ,
-      ) {
-         AndroidView(factory = {
+            .fillMaxSize(),
+         factory = {
             WebView(it).apply {
                layoutParams = ViewGroup.LayoutParams(
                   ViewGroup.LayoutParams.MATCH_PARENT,
                   ViewGroup.LayoutParams.MATCH_PARENT
                )
-               webViewClient = object : WebViewClient() {
+
+               settings.javaScriptEnabled = true
+               webViewClient = object: WebViewClient() {
                   override fun shouldOverrideUrlLoading(
                      view: WebView?,
                      request: WebResourceRequest?
@@ -50,15 +67,33 @@ fun VideoPlayer(
                      return true
                   }
                }
+               webChromeClient = object : WebChromeClient() {
+                  var customView: View? = null
+                  override fun onShowCustomView(view: View?, callback: CustomViewCallback?) {
+                     super.onShowCustomView(view, callback)
+                     isFullscreen.value = true
+                     if (this.customView != null) {
+                        onHideCustomView()
+                        return
+                     }
+                     this.customView = view
+                     (activity.window.decorView as FrameLayout).addView(this.customView,
+                        FrameLayout.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.MATCH_PARENT))
+                  }
+
+                  override fun onHideCustomView() {
+                     super.onHideCustomView()
+                     isFullscreen.value = false
+                     (activity.window.decorView as FrameLayout).removeView(this.customView)
+                     this.customView = null
+                  }
+               }
                loadUrl(url)
             }
-         },
-            update = {
-               it.loadUrl(url)
-               it.settings.javaScriptEnabled = true
-            }
-         )
-      }
+      },
+      )
+   }
 
 
-}
