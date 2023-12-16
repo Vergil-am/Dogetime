790dce54d974bac6a04d76ef7aa721a345a7d4a1
amine <madmanamine2@gmail.com>
2023-12-16 21:55:44 +0100

    Working on local database

--- app/src/main/java/com/example/kotlinmovieapp/local/repository/ListRepository.kt
+++ app/src/main/java/com/example/kotlinmovieapp/local/repository/ListRepository.kt
@@ -6,4 +6,13 @@ class ListRepository (
     private val list : ListDao
 ){
     // TODO
+
+    suspend fun getAllLists() : List<com.example.kotlinmovieapp.local.entities.List> {
+        return list.getLists()
+    }
+
+    suspend fun createList(newList: com.example.kotlinmovieapp.local.entities.List) {
+        return list.createList(newList)
+    }
+
 }
