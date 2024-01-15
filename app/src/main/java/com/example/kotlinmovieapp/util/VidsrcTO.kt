03288e5c11c500ecdf5afb194b4e9bf25ffe8ace
amine <madmanamine2@gmail.com>
2024-01-15 22:28:51 +0100

    writing vidsrc parser

--- app/src/main/java/com/example/kotlinmovieapp/util/VidsrcTO.kt
+++ app/src/main/java/com/example/kotlinmovieapp/util/VidsrcTO.kt
@@ -2,11 +2,17 @@ package com.example.kotlinmovieapp.util
 
 import android.util.Log
 import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import org.jsoup.Jsoup
+
+data class Source(
+    val id: String,
+    val title: String
+)
 
 class VidsrcTO {
 
@@ -17,34 +23,35 @@ class VidsrcTO {
                 val doc = Jsoup.connect(url).get().body()
                 val dataId = doc.selectFirst("a[data-id]")?.attr("data-id")
                 if (dataId != null) {
-                    Log.e("this ran", "")
                     getSources(dataId)
                 }
             } catch (e: Exception) {
                 e.printStackTrace()
             }
-
         }
+
 
     }
 
     private suspend fun getSources(dataId: String) {
-        withContext(Dispatchers.IO) {
-            val url = "${Constants.VIDSRC_MULTI}/ajax/embed/episode/$dataId/sources"
-            val request = Request.Builder().url(url).build()
-            try {
-                val response = OkHttpClient().newCall(request).execute().use { res ->
-                    val body = res.body?.string() ?: ""
-                    val data = Gson().fromJson(body, Map::class.java)
-                    Log.e("result", data.toString())
-                    return@use res
-                }
-                return@withContext response
-            } catch (e: Exception) {
-                e.printStackTrace()
+        val url = "${Constants.VIDSRC_MULTI}/ajax/embed/episode/$dataId/sources"
+        val request = Request.Builder().url(url).build()
+
+        val res = try {
+
+            val response = OkHttpClient().newCall(request).execute().use { res ->
+                val body = res.body?.string() ?: ""
+                val type = object : TypeToken<List<Source>>() {}.type
+                val result: List<Source> =  Gson().fromJson(body, type)
+                Log.e("Result", result.toString())
+
+
             }
-
-
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
+
+        TODO("I need to finish this")
     }
+
 }
